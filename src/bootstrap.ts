8bd6aef5fcda3a833f220b1044cb39b42e04c92a
Zeeshan Ayaz <zeeshanayaz1@gmail.com>
2016-05-06 22:02:43 +0500

    main page routing

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,15 +1,38 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
+import {HomeComponent} from './main';
+import {CountriesComponent} from './countries/countriesComponent';
 
 
 @Component({
   selector: 'app',
   template: `
   	<h1>Countries and Capitals</h1>
+    <router-outlet><router-outlet>
   `,
+  styleUrls:[''],
+  directives: [ROUTER_DIRECTIVES,HomeComponent],
+  providers:[ROUTER_PROVIDERS]
 })
+
+@RouteConfig([
+  {
+    path: './main',
+    name: 'Home',
+    component: HomeComponent,
+    useAsDefault: true
+  },
+  {
+    path: './countries',
+    name: 'Countries',
+    component: CountriesComponent
+  }
+])
+
 export class App {}
 
 bootstrap(App)
   .then(success => console.log('Kicking off Countries and Capitals'))
   .catch(error => console.log(error));
+  
